05d1338c8f9563bdcf2aa2bf0e10ad50269a9ef3
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2020-12-20 20:45:34 -0500

    create func for format input to first character capitalized form

--- processUpdates.py
+++ processUpdates.py
@@ -14,3 +14,13 @@ def formatInput(textLine):
 	textLine = " ".join(wordList)
 	
 	return textLine
+
+def capitalizeInput(sentence):
+	"""
+	This function will take a sentence(string), convert it to the proper format.
+	The first character is capitalized.
+
+	Note: SouthKorea will become SouthKorea,
+	since we are only changing the first letter
+	"""
+	return " ".join(list(map(lambda x : x[0].upper() + x[1:], sentence.strip().split())))
