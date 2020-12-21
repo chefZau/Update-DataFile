930d8a14be190d32963ddf682a36bd9c9c9ade8f
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2020-12-20 20:44:13 -0500

    add func for format user input, clean extra speace and lower text

--- processUpdates.py
+++ processUpdates.py
@@ -1,2 +1,16 @@
 import os
 from catalogue import *
+
+def formatInput(textLine):
+	"""
+	the purpose of this funciton is to clean
+	leading and trailing spaces, including 
+	cases when multiple spaces separate words
+	
+	:param: textLine: the input text
+	"""
+	textLine = textLine.upper().strip()
+	wordList = textLine.split()
+	textLine = " ".join(wordList)
+	
+	return textLine
